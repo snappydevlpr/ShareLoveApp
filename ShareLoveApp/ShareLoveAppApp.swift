9656631fb0bec353526ad231a88aff45adc9fa44
Austin Suarez <austinmsuarez@gmail.com>
2022-02-05 18:46:06 -0600

    Initial Commit

---
+++ ShareLoveApp/ShareLoveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLoveAppApp.swift
+//  ShareLoveApp
+//
+//  Created by Austin Suarez on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ShareLoveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
